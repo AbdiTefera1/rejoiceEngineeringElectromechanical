46da4b5fcdd6734caa92957bfbff5f2db271e1e1
Abdi Tefera <abditefera11@gmail.com>
2025-02-17 00:13:51 +0300

    contact page is created but isn't completed yet

---
+++ app/contact/page.tsx
@@ -0,0 +1,19 @@
+import FooterCTA from '@/components/FooterCTA';
+import Header from '@/components/Header';
+import TopBar from '@/components/TopBar';
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>
+        <TopBar/>
+        <Header/>
+        <div className='h-screen'>
+            <h1>CONTACT PAGE</h1>
+        </div>
+        <FooterCTA/>
+    </div>
+  )
+}
+
+export default ContactPage;
